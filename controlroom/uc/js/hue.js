58c0773c6deadd012678d72ac031a8856d9318e8
TheStartLier <bennycockx@thestart.be>
2024-05-17 19:05:06 +0200

    Update hue.js

--- controlroom/uc/js/hue.js
+++ controlroom/uc/js/hue.js
@@ -885,7 +885,8 @@ var colorChecker = setInterval(async function(){
       const config = {
           method: 'get',
           url: `${hueIp}/api/FswU2OL6HzX9kDVwNNL9GTMjvD4wdQVeeIOuMqpE/groups/20`,
-	      headers:{"Access-Control-Allow-Origin": "*",
+	      headers:{'Access-Control-Allow-Origin': '*',
+        		'Content-Type': 'application/json',
 		      "crossorigin":true}
       };
       let res = await axios(config)
