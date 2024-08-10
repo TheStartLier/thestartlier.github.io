334817b253dcb91c5ab5ab282ec782c29ebdab74
TheStartLier <bennycockx@thestart.be>
2024-08-10 14:25:41 +0200

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -1,10 +1,10 @@
 $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
 
 var saveddata = [];
-var queueddata = [];
 var verjaardagen = [];
 var vlaaikes = [];
 var engels = [];
+var alreadyLoading = false;
 
 setInterval(fetchBookingDetails, 5000);
 
@@ -26,43 +26,53 @@ function fetchBookingDetails(){
         $(".box_icons", bookingslot).prepend('<span>EN</span>');
       }
     }else{
-      if(queueddata.indexOf(bookingid) < 0){
-        queueddata.push(bookingid);
-        setTimeout(function(){
-          $.ajax({
-            type: "POST",
-            url: "https://web-2556h.bookeo.com/bookeo/book_viewEventSlotDetails.html",
-            data: {
-              "rid": battr[1],
-              "esid": battr[2],
-              "date": bdate,
-              "refresh_schedule_next": true,
-              "refresh_startDate": bdate,
-              "ncs": _axiom_nocsrfid,
-              "ncs2": _axiom_nocsrfid2
-            },
-            success: function(data){
-              saveddata.push(bookingid);
-              let queueindex = queueddata.indexOf(bookingid);
-              queueddata.splice(queueindex, 1);
-              if(data.indexOf("Verjaardag") > 0){
-                $(".box_icons", bookingslot).prepend('<i class="fa fa-birthday-cake"></i>');
-                verjaardagen.push(bookingid);
-              }
-              if(data.indexOf("Lekker. Dit mag je ons allemaal serveren") > 0){
-                $(".box_icons", bookingslot).prepend('<i class="fa fa-cutlery"></i>');
-                vlaaikes.push(bookingid);
-              }
-              if(data.indexOf("Engels") > 0){
-                $(".box_icons", bookingslot).prepend('<span>EN</span>');
-                engels.push(bookingid);
-              }
-            }
-          });
-        }, i * 1000);
-      }
+      fetchBookingDetails(battr, bdate, bookingid, bookingslot);
     }
   })
+}
+
+function fetchBookingDetails(battr, bdate, bookingid, bookingslot){
+  if(alreadyLoading){
+    setTimeout(function(){
+      fetchBookingDetails(battr, bdate, bookingid, bookingslot);
+    }, 1000);
+  }else{
+    if(saveddata.indexOf(bookingid) < 0){
+      alreadyLoading = true;
+      $.ajax({
+        type: "POST",
+        url: "https://web-2556h.bookeo.com/bookeo/book_viewEventSlotDetails.html",
+        data: {
+          "rid": battr[1],
+          "esid": battr[2],
+          "date": bdate,
+          "refresh_schedule_next": true,
+          "refresh_startDate": bdate,
+          "ncs": _axiom_nocsrfid,
+          "ncs2": _axiom_nocsrfid2
+        },
+        success: function(data){
+          alreadyLoading = false;
+          saveddata.push(bookingid);
+          if(data.indexOf("Verjaardag") > 0){
+            $(".box_icons", bookingslot).prepend('<i class="fa fa-birthday-cake"></i>');
+            verjaardagen.push(bookingid);
+          }
+          if(data.indexOf("Lekker. Dit mag je ons allemaal serveren") > 0){
+            $(".box_icons", bookingslot).prepend('<i class="fa fa-cutlery"></i>');
+            vlaaikes.push(bookingid);
+          }
+          if(data.indexOf("Engels") > 0){
+            $(".box_icons", bookingslot).prepend('<span>EN</span>');
+            engels.push(bookingid);
+          }
+        },
+        error: function(){
+          alreadyLoading = false;
+        }
+      });
+    }
+  }
 }
 
 // Customer history
