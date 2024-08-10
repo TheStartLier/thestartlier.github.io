535da6b838cca25508a5bec3ca1dd8dd8a28a325
TheStartLier <bennycockx@thestart.be>
2024-08-10 19:04:19 +0200

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -68,7 +68,7 @@ function fetchBookingDetails(battr, bdate, bookingid, bookingslot){
             engels.push(bookingid);
           }
           let ervaring = data.split("Hoeveel escape rooms heeft je team")[1];
-          ervaring = ervaring.substring(ervaring.indexOf("<td >")+1, ervaring.indexOf("</td>"));
+          ervaring = ervaring.substring(ervaring.indexOf("<td >")+5, ervaring.indexOf("</td>")).trim();
           if(ervaring.length > 15){
             ervaring = ervaring.substring(0, 15) + "...";
           }
