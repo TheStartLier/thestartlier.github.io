9f140aa5fd029cf3a6b33b98631f88ded3cb1f36
TheStartLier <bennycockx@thestart.be>
2025-10-04 14:45:10 +0200

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -157,13 +157,13 @@ async function loadWaivers(){
             data.forEach(function(item, i) {
               let print = "";
               if(item.game_played == "uc"){
+                // Capitalize every first letter
                  let splitStr1 = item.first_name.toLowerCase().split(' ');
                  let splitStr2 = item.last_name.toLowerCase().split(' ');
                  let splitStr = splitStr1.concat(splitStr2);
                  for (var i = 0; i < splitStr.length; i++) {
                      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
                  }
-                 // Directly return the joined string
                  let naam = splitStr.join('%20'); 
                 print = '<a target="_blank" href="https://intern.thestart.be/strafblad.php?name=' + naam + '">Strafblad</a>';
               }
