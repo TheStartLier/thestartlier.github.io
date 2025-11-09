7f1f083619c08182b79b88a56f5f81eaf96c9782
TheStartLier <bennycockx@thestart.be>
2025-11-09 15:26:28 +0100

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -269,7 +269,9 @@ function buildIcons(){
 
             // Kids
             if(item.productID = "42556W7PC9W1904AFECC0E" && value.name.indexOf("Vier je een verjaardag bij TheStart?") > -1){
-              let jarige = value.value;
+              let jarige = value.value.replace(/[\u00A0-\u9999<>\&]/g, function(i) {
+                 return '&#'+i.charCodeAt(0)+';';
+              });
               $(".ctev_in", bookingslot).append(`<div class="print" onclick="event.stopPropagation();window.open('https://intern.thestart.be/strafblad.php?name=` 
                                                   + jarige + `&lang=kids','_blank');" title="Print Strafblad."><i class="fa fa-print"></i></div>`);
             }
