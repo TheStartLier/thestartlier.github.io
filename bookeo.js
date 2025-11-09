57161c6116cc3b2cf359228077674ad10e26d965
TheStartLier <bennycockx@thestart.be>
2025-11-09 14:47:09 +0100

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -267,6 +267,13 @@ function buildIcons(){
               $(".box_icons", bookingslot).prepend('<i class="fa fa-birthday-cake"></i>');
             }
 
+            // Kids
+            if(item.productID = "42556W7PC9W1904AFECC0E" && value.name.indexOf("Vier je een verjaardag bij TheStart?" > -1){
+              let jarige = value.value;
+              $(".ctev_in", bookingslot).append(`<div class="print" onclick="event.stopPropagation();window.open('https://intern.thestart.be/strafblad.php?name=` 
+                                                  + jarige + `&lang=kids','_blank');" title="Print Strafblad."><i class="fa fa-print"></i></div>`);
+            }
+
             if(value.value.indexOf("Lekker. Dit mag je ons allemaal serveren") > -1 || 
               value.value.indexOf("Ja, perfect voor na onze escape") > -1 || 
               value.value.indexOf("Ja, lekker!") > -1 || 
