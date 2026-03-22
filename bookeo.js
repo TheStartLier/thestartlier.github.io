da5bc5554bc65675b736acc0a4fc13adb0c1ec99
TheStartLier <bennycockx@thestart.be>
2026-03-22 17:01:07 +0100

    Update bookeo.js

--- bookeo.js
+++ bookeo.js
@@ -480,12 +480,18 @@ if(window.location.href.includes("autodownload=true")){
                 }
                 document.querySelector("span[data-testid='minutes']:first-of-type").innerText = newVal;
                 interv = setInterval(function(){
-                  if(document.querySelector("div[class*=styles__ButtonCover] button:first-of-type")){
+                  if(document.querySelector("div[class*=footerRight] button:last-of-type")){
                     clearInterval(interv);
-                    document.querySelector("div[class*=styles__ButtonCover] button:first-of-type").click();
-                    setTimeout(function(){
-                      history.go(-1);
-                    }, 30000);
+                    document.querySelector("div[class*=footerRight] button:last-of-type").click();
+                    interv = setInterval(function(){
+                      if(document.querySelector("div[class*=styles__ButtonCover] button:first-of-type")){
+                        clearInterval(interv);
+                        document.querySelector("div[class*=styles__ButtonCover] button:first-of-type").click();
+                        setTimeout(function(){
+                          history.go(-1);
+                        }, 30000);
+                      }
+                    }, 500);
                   }
                 }, 500);
               }
